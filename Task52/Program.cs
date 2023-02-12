8f55972bfccf14c56d73d43b0ba523738af49865
Kamila <kamilla.adamson@gmail.com>
2023-02-13 01:48:02 +0600

    Added a Task52

---
+++ Task52/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+
+int[,] getArray()
+{
+  int[,] array = new int[5, 5];
+  Random random = new Random();
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = random.Next(1, 10);
+      Console.Write(array[i, j] + "\t");
+    }
+    Console.WriteLine();
+  }
+  return array;
+}
+
+void getArithmeticMean(int[,] array)
+
+{
+  for (int j = 0; j < array.GetLength(1); j++)
+  {
+    float sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+      sum += array[i, j];
+    }
+    sum = sum / array.GetLength(1);
+    Console.WriteLine(sum);
+  }
+
+}
+
+int[,] array = getArray();
+Console.WriteLine();
+getArithmeticMean(array);
